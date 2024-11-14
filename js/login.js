9dd7f9e3ff9188ee36afc7b2036b4cdefee2f202
0and4 <iwonchu215@gmail.com>
2024-11-14 17:24:08 +0900

    메인페이지 사이드바 역할표시(추후 웹템플릿으로 통합)

--- js/login.js
+++ js/login.js
@@ -27,7 +27,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
       const data = await response.json();
       if (data.message === "Login successful") {
-        document.cookie = `csrfToken=${data.csrfToken}; path=/; secure`; // 서버에서 받은 CSRF 토큰을 쿠키에 저장
+        //document.cookie = `csrfToken=${data.csrfToken}; path=/; secure`; // 서버에서 받은 CSRF 토큰을 쿠키에 저장
+        localStorage.setItem("role", data.role);
         localStorage.setItem("isLoggedIn", "true"); // 로그인 상태 저장
         window.location.href = "../../html/main/main.html"; // 메인 페이지로 이동
         console.log(document.cookie);
